37f7fc916c2737185ca78ba224956e3dc8fcf6fa
TousifProvat <tousifprovat15@gmail.com>
2023-03-03 02:51:11 +0600

    match phone number with regex

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,37 +1,46 @@
 const bcrypt = require('bcryptjs');
-const mongoose =require('mongoose');
-const UserSchema=new mongoose.Schema({
-    name:{type:String,max:30,required:true},
-    email:{type:String,unique:true,required: true},
-    phone_number:{type:String,max: 13},
-    password:{type:String,max:6},
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, max: 30, required: true },
+    email: { type: String, unique: true, required: true },
+    phone_number: { type: String, max: [13, 'Must be a valid phone number'] },
+    password: { type: String, max: 6 },
 
-    role:{
-        type:String,
-        default:"ACO",
-        // ACO=Account Owner,BM=Brand Manager,OM=OutletManager
-        enum:["ACO","BM","OM"],
-        required:true
-    }
-
-},{versionKey:false},{timestamps:true})
+    role: {
+      type: String,
+      default: 'ACO',
+      // ACO=Account Owner,BM=Brand Manager,OM=OutletManager
+      enum: ['ACO', 'BM', 'OM'],
+      required: true,
+    },
+  },
+  { versionKey: false },
+  { timestamps: true }
+);
 
 // Password Hash Function using Bycryptjs
 
 UserSchema.pre('save', async function hashPassword(next) {
-    if (this.isModified('password')) {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-    }
-    next();
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
 });
 
 UserSchema.methods = {
-    async authenticate(password) {
-        return await bcrypt.compare(password, this.password);
-    },
+  async authenticate(password) {
+    return await bcrypt.compare(password, this.password);
+  },
 };
 
-const UserModel=mongoose.model('user',UserSchema)
+//Validations
+user.path('phone_number').validate(function (value) {
+  const regex = /^\d{13}$/; // regular expression to match 11 digits
+  return regex.test(phoneNumber);
+}, 'Must be a valid phone number');
 
-module.exports=UserModel
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
