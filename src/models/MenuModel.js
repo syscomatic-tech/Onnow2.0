21aca969f92231c0e37e1139385161b6db407ec5
Zahed75 <zahedhasan.cs@gmail.com>
2023-03-04 01:42:33 +0600

    menu item schema done

--- src/models/MenuModel.js
+++ src/models/MenuModel.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
 const MenuSchema = new mongoose.Schema({
-  menuName: {
-    type: String,
-    required: [true, 'Must set a menu name'],
-    max: [20, 'Menu name cannot be greater than 20 characters'],
-  },
-  items: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'menutItem',
+    menuName: {
+        type: String,
+        required: [true, 'Must set a menu name'],
+        max: [20, 'Menu name cannot be greater than 20 characters'],
     },
-  ],
+
+    isActive: {
+        type: Boolean,
+        default: true,
+
+    },
+    offLineTime: {
+        type: String,
+        default: ''
+
+    },
+
+
 });
 
-const MenuModel = mongoose.model('Menu', MenuSchema);
+const MenuModel = mongoose.model('menu', MenuSchema);
 
 module.exports = MenuModel;
