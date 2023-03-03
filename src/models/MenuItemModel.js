8b785b1cf5c5170b9372d877e1197b7325c63d74
TousifProvat <tousifprovat15@gmail.com>
2023-03-04 01:50:09 +0600

    added two fields in outlet and typo fix

--- src/models/MenuItemModel.js
+++ src/models/MenuItemModel.js
@@ -1,42 +1,37 @@
 const mongoose = require('mongoose');
-const {models} = require("mongoose");
+const { models } = require('mongoose');
 
 const MenuItemSchema = new mongoose.Schema({
-    menu: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'menu',
-        required: [true, 'Must select a menu']
-    },
+  menu: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'menu',
+    required: [true, 'Must select a menu'],
+  },
 
-    itemName: {
-        type: String,
-        required: [true, 'Must set a name'],
-
-    },
-    description: {
-        type: String,
-        max: [600, 'Your item description cannot be grater then 600']
-
-    },
-    itemImage: {
-        type: String,
-        required: [true, 'Must  set a item image']
-    },
-    itemPrice: {
-        type: Number,
-        max: [50000, 'Your item price  cannot be greater then 50000'],
-
-    },
-    isActive: {
-        type: Boolean,
-        default: true,
-
-    },
-    offLineTime: {
-        type: String,
-        default: ''
-    }
-})
-const MenuItemModel = mongoose.model('menuItem', MenuItemSchema)
+  itemName: {
+    type: String,
+    required: [true, 'Must set a name'],
+  },
+  description: {
+    type: String,
+    max: [600, 'Your item description cannot be grater then 600'],
+  },
+  itemImage: {
+    type: String,
+    required: [true, 'Must  set a item image'],
+  },
+  itemPrice: {
+    type: Number,
+    max: [50000, 'Your item price  cannot be greater then 50000'],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  offlineTime: {
+    type: String,
+    default: '',
+  },
+});
+const MenuItemModel = mongoose.model('menuItem', MenuItemSchema);
 module.exports = MenuItemModel;
-
--- src/models/MenuModel.js
+++ src/models/MenuModel.js
@@ -1,23 +1,19 @@
 const mongoose = require('mongoose');
 const MenuSchema = new mongoose.Schema({
-    menuName: {
-        type: String,
-        required: [true, 'Must set a menu name'],
-        max: [20, 'Menu name cannot be greater than 20 characters'],
-    },
+  menuName: {
+    type: String,
+    required: [true, 'Must set a menu name'],
+    max: [20, 'Menu name cannot be greater than 20 characters'],
+  },
 
-    isActive: {
-        type: Boolean,
-        default: true,
-
-    },
-    offLineTime: {
-        type: String,
-        default: ''
-
-    },
-
-
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  offlineTime: {
+    type: String,
+    default: '',
+  },
 });
 
 const MenuModel = mongoose.model('menu', MenuSchema);
