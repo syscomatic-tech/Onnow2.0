8d99657819d246de4857a935f8b1e494cfcc6e3e
TousifProvat <tousifprovat15@gmail.com>
2023-03-04 01:27:01 +0600

    menu model added

--- src/models/MenuModel.js
+++ src/models/MenuModel.js
@@ -1,2 +1,18 @@
-const mongoose=require('mongoose');
-const MenuSchema=new mongoose.Schema
+const mongoose = require('mongoose');
+const MenuSchema = new mongoose.Schema({
+  menuName: {
+    type: String,
+    required: [true, 'Must set a menu name'],
+    max: [20, 'Menu name cannot be greater than 20 characters'],
+  },
+  items: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'menutItem',
+    },
+  ],
+});
+
+const MenuModel = mongoose.model('Menu', MenuSchema);
+
+module.exports = MenuModel;
