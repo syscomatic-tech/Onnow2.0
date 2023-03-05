36c41f912706b4c5c2b3c22841e05d093c1cef4f
TousifProvat <tousifprovat15@gmail.com>
2023-03-05 11:58:40 +0600

    feat: export all model from models/index.js

---
+++ src/models/index.js
@@ -0,0 +1,23 @@
+const User = require('./UserModel');
+const Customer = require('./CustomerModel');
+const BrandManager = require('./BrandManagerModel');
+const OutletManager = require('./OutletManagerModel');
+const Brand = require('./BrandModel');
+const Outlet = require('./OutletModel');
+const Menu = require('./MenuModel');
+const MenuItem = require('./MenuItemModel');
+const Order = require('./OrderModel');
+const Discount = require('./DiscountModel');
+
+module.exports = {
+  User,
+  Customer,
+  BrandManager,
+  OutletManager,
+  Brand,
+  Outlet,
+  Menu,
+  MenuItem,
+  Order,
+  Discount,
+};
