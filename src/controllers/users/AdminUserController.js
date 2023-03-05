567667e425911789222d6001ddb5ddd00cf9d9f1
TousifProvat <tousifprovat15@gmail.com>
2023-03-05 13:41:08 +0600

    conflict fix

--- src/controllers/users/AdminUserController.js
+++ src/controllers/users/AdminUserController.js
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
-const AdminModel=require("../../models/UserModel");
-const AdminCreateService=require('../../services/User/AdminAuthService');
-=======
 const express = require('express');
-const models = require('../../models');
->>>>>>> 8107e828c9d1a807fe6bde837531add16189b337
-
-const { AdminModel } = models;
+const { AdminModel } = require('../../models');
 
 const adminAuthService = require('../../services/User/AdminAuthService');
 
 const router = express.Router();
 // Admin Registration
 
-<<<<<<< HEAD
-exports.Registration=async (req,res)=>{
-    let result=await AdminCreateService(req, AdminModel)
-
-    res.status(200).json(result)
-}
-=======
 const Registration = async (req, res, next) => {
   try {
     const user = await adminAuthService.registerAdmin(req.body, AdminModel);
@@ -37,4 +23,3 @@ const Registration = async (req, res, next) => {
 router.post('/reg', Registration);
 
 module.exports = router;
->>>>>>> 8107e828c9d1a807fe6bde837531add16189b337
