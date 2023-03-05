337df83dd0bd4d06eb16420cffde96e1889ddfb8
Zahed75 <zahedhasan.cs@gmail.com>
2023-03-05 13:44:58 +0600

    admin reg resolved

--- src/services/User/AdminAuthService.js
+++ src/services/User/AdminAuthService.js
@@ -1,54 +1,37 @@
 const generateOTP = require('../OTPService/OTP-Generate');
 const SendOTP = require('../OTPService/SendOTP');
-const {BadRequest} = require('../../utility/errors');
+const { BadRequest } = require('../../utility/errors');
 
 const registerAdmin = async (body, UserModel) => {
-    const {name, email, phoneNumber, password} = body;
+    const { name, email, phoneNumber, password } = body;
 
     if (!password) {
         throw new BadRequest('Please enter a password');
     }
 
-
     //checks if user exists
-    const userExists = await UserModel.findOne({email});
+    const userExists = await UserModel.findOne({ email });
 
+    if (userExists) {
+        throw new BadRequest('User already exists');
+    }
+
+    // generate and send OTP
+    const otp = generateOTP();
+    await SendOTP(email, otp);
 
     // save the OTP in the user document
-    const newUser = new AdminModel({
-        email, otp,
+    const newUser = new UserModel({
+        name,
+        phoneNumber,
+        email,
+        password,
+        otp,
     });
-    await newUser.save();
 
-    // return res.status(200).json({ message: 'OTP sent successfully' });
-    return {status: "Success", message: "OTP sent successfully"}
-}
-catch
-(error)
-{
-    console.log(error);
-    // return res.status(500).json({ error: 'Something went wrong' });
-    return {status: "Failed", err: "Something went wrong"}
-}
+    const user = await newUser.save();
 
-if (userExists) {
-    throw new BadRequest('User already exists');
-}
+    return user;
+};
 
-// generate and send OTP
-const otp = generateOTP();
-await SendOTP(email, otp);
-
-// save the OTP in the user document
-const newUser = new UserModel({
-    name, phoneNumber, email, password, otp,
-});
-
-const user = await newUser.save();
-
-return user;
-
-}
-;
-
-module.exports = {registerAdmin};
+module.exports = { registerAdmin };
