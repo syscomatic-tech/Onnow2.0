7d2782bc0031ee4242af9d126b2fe6c1446d8c41
TousifProvat <tousifprovat15@gmail.com>
2023-03-05 16:25:41 +0600

    feat: Verify OTP service

--- src/services/OTPService/VerifyOTP.js
+++ src/services/OTPService/VerifyOTP.js
@@ -1,8 +1,23 @@
+const { AdminModel } = require('../../models');
+const { NotFound, BadRequest } = require('../../utility/errors');
 
-const VerifyOTP = (otp1, otp2) => {
-    // verify the OTPs
-    return otp1 === otp2;
+const VerifyOTP = async (body) => {
+  const { email, otp } = body;
+  // verify the OTPs
+
+  const userExists = await AdminModel.findOne({ email });
+
+  if (!userExists) {
+    throw new NotFound('User does not exist with this email address');
+  }
+
+  if (!userExists.otp === otp) {
+    throw new BadRequest('Invalid OTP');
+  }
+
+  await AdminModel.updateOne({ email }, { $unset: { otp: 1 } });
+
+  return userExists;
 };
 
-
-module.exports= {VerifyOTP};
+module.exports = { VerifyOTP };
